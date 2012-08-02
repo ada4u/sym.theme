05fd78db1f0805447e547ceb828536b3d32a812e
Armando Andrade <aada4u@gmail.com>
2012-08-01 19:10:05 -0500

    initial for plone 4.2

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.0'
+
+setup(name='sym.theme',
+      version=version,
+      description="A diazo based theme for symab",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['sym'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
